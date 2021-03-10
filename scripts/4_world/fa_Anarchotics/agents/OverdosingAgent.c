347eab9dbfbc17312db7343f347632d3c067a03c
FatherAnarchy <70991592+GickiAnarchy@users.noreply.github.com>
2021-03-09 18:35:35 -0600

    update OverdosingAgent.c

--- scripts/4_world/fa_Anarchotics/agents/OverdosingAgent.c
+++ scripts/4_world/fa_Anarchotics/agents/OverdosingAgent.c
@@ -7,8 +7,8 @@ class OverdosingAgent: AgentBase
 		m_TransferabilityIn		= 0;//to the player
 		m_TransferabilityOut	= 0;//from the player
 		m_AntibioticsResistance = 1;//[0..1], 0 means antibiotics have full effect, 1 means no effect
-		m_MaxCount 				= 1000;
-		m_Potency = EStatLevels.MEDIUM;//grow when player's immune system is at this level or lower
-		m_DieOffSpeed = 0.83;//how fast the agent dies off when not potent enough to grow(per sec)
+		m_MaxCount 				= 100;
+		m_Potency = EStatLevels.CRITICAL;//grow when player's immune system is at this level or lower
+		m_DieOffSpeed = 0.10;//how fast the agent dies off when not potent enough to grow(per sec)
 	}
 }
