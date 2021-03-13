2db898e7cd01883cbceda3e5b70601b34375ccf1
FatherAnarchy <fatheranarchy@programmer.net>
2021-03-13 08:34:19 -0600

    Setup
    Setting up on laptop and other stuff

--- scripts/4_world/fa_Anarchotics/entities/Drugs/fa_meth.c
+++ scripts/4_world/fa_Anarchotics/entities/Drugs/fa_meth.c
@@ -1,8 +1,19 @@
 class Meth extends FA_DrugBase
 {
 	void Meth()
-	{}
+	{
+		
+		InsertAgent(AnarchoticsAgents.OVERDOSING, 10);
+		
+	};
 	
+	
+	overrride void SetActions()
+	{
+		super.SetActions();
+		
+		AddAction(ActionEatSmall);
+	}
 	
 
 };
