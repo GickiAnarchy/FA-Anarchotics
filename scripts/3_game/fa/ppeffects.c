29c7f2d202b8a65cbfc7f3d6e5f451a76dcfa1d2
FatherAnarchy <70991592+GickiAnarchy@users.noreply.github.com>
2021-04-08 14:01:35 -0500

    update ppeffects.c

--- scripts/3_game/fa/ppeffects.c
+++ scripts/3_game/fa/ppeffects.c
@@ -1,11 +1,16 @@
 class FAEffects extends PPEffects
 {
+  //Vignette
 	static int 		m_VignetteTrippy;
 	static float	m_VignetteTrippy_R		 	= 10;
 	static float	m_VignetteTrippy_G	 		= 150;
 	static float	m_VignetteTrippy_B		 	= 10;
 	static float	m_VignetteTrippy_A	 		= 0.2;
 	static float vigColor[4];
+	
+	//Color
+	static int m_ColorTrippy;
+	
 	
 	override static void Init()
 	{
@@ -15,24 +20,37 @@ class FAEffects extends PPEffects
 		vigColor[1] = m_VignetteTrippy_G;
 		vigColor[2] = m_VignetteTrippy_B;
 		vigColor[3] = m_VignetteTrippy_A;
-		//SetTrippyVignette();
+		//
+		m_ColorTrippy = RegisterColorEffect();
 	}
+	
 	
 	static void SetTrippyVignette(float value = 0.9) //TODO- Make randomized
 	{
 		SetVignetteEffectValue(m_VignetteTrippy, value, 20,225,20,0.5); //int, intensity, r,g,b,a
 		UpdateVignette();
 	}
-	
-	static void EnableTrippyVignette()
-	{
-		SetVignette(0.9, vigColor[0],vigColor[1],vigColor[2],vigColor[3]);
-	}
-	
 	static void DisableTrippyVignette()
 	{
 		SetVignetteEffectValue(m_VignetteTrippy, 0,0,0,0,0); //int, intensity, r,g,b,a
 		UpdateVignette();
+	}
+	
+	
+	static void EnableTrippyColor()
+	{
+	  int rgb[3];
+	  rgb[0] = Math.RandomInt(1,254);
+	  rgb[1] = Math.RandomInt(1,254);
+	  rgb[2] = Math.RandomInt(1,254);
+	  
+	  SetColorValue(m_ColorTrippy, rgb[0], rgb[1], rgb[2], 0.7, 0.6);
+	  UpdateColor();
+	}
+	static void DisableTrippyColor()
+	{
+	  SetColorValue(m_ColorTrippy,0,0,0,0,0);
+	  UpdateColor();
 	}
 	
 };
