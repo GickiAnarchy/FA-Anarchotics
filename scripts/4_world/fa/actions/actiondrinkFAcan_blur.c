92b9e6dcdaf4e9435c6bc8ce1d814c917b8caf78
FatherAnarchy <70991592+GickiAnarchy@users.noreply.github.com>
2021-04-09 10:49:21 -0500

    Add files via upload

---
+++ scripts/4_world/fa/actions/actiondrinkFAcan_blur.c
@@ -0,0 +1,19 @@
+class ActionDrinkFACan_Blur: ActionDrink
+{
+	void ActionDrinkFACan_Blur()
+	{
+		m_CommandUID = DayZPlayerConstants.CMD_ACTIONMOD_DRINK;
+		m_CommandUIDProne = DayZPlayerConstants.CMD_ACTIONFB_DRINK;
+	}
+	
+	override string GetText()
+	{
+		return "#drink";
+	}
+	
+	override void ApplyModifiers( ActionData action_data )
+	{
+	  action_data.m_Player.InsertAgent(FAagents.TRIPPING,100);
+	}
+	
+};
